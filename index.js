7d2fdbc4ac27c1f0dbae426f22a36dfae7ac5244
ZhangZY <346114608@qq.com>
2017-07-18 12:38:34 +0800

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/',function(req,res){
+	res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection',function(socket){
+	// console.log('a user connected');
+	// socket.on('disconnect',function(){
+	// 	console.log('user disconnected');
+	// });
+	socket.on('chat message',function(msg){
+		io.emit('chat message',msg);
+	});
+});
+
+io.emit('some event',{for: 'everyone'});
+
+io.on('connection',function(socket){
+	socket.broadcast.emit('hi');
+});
+
+http.listen(3000,function(){
+	console.log('listen on port:3000');
+});
